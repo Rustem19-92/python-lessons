50193e315598bee95a23bc59bfc17e0e379f5832
Rustem19-92 <cranky.228@gmail.com>
2025-04-29 14:23:56 +0300

    Update less14.py

--- less14.py
+++ less14.py
@@ -15,10 +15,12 @@ favorite_foods = {
     "Shahzod": "Manti",
     "Bekzod": "Shashlik",
     "Lola": "Palov",
-    "Diana": "Lag'mon"
+    "Abdumalik": "Lag'mon"
 }
 
 
 print("Alining sevimli taomi:", favorite_foods["Ali"])
 print("Shahzodning sevimli taomi:", favorite_foods["Shahzod"])
 print("Bekzodning sevimli taomi:", favorite_foods["Bekzod"])
+print("Lolaning sevimli taomi:", favorite_foods["Lola"])
+print("Abdumalikning sevimli taomi:", favorite_foods["Abdumalik"])
