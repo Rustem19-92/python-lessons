4980b41540a2e86066afb17265eabcd4f8c0378d
Rustem19-92 <cranky.228@gmail.com>
2025-04-29 15:09:44 +0300

    less14-16

---
+++ less14.py
@@ -0,0 +1,24 @@
+
+otam = {
+    "ismi": "Mavlutdin",
+    "tugilgan_yili": 1954,
+    "shahri": "Samarqand viloyati"
+}
+
+
+print(f"Otamning ismi {otam['ismi']}, {otam['tugilgan_yili']}-yilda, {otam['shahri']}da tug'ilgan.")
+
+
+
+favorite_foods = {
+    "Ali": "Osh",
+    "Samar": "Manti",
+    "Bekzod": "Shashlik",
+    "Lola": "Palov",
+    "Diana": "Lag'mon"
+}
+
+
+print("Alining sevimli taomi:", favorite_foods["Ali"])
+print("Samarning sevimli taomi:", favorite_foods["Samar"])
+print("Bekzodning sevimli taomi:", favorite_foods["Bekzod"])
