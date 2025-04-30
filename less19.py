47e70be92749b5acbb7bb9311d3deaf75dd7941a
Rustem19-92 <cranky.228@gmail.com>
2025-04-30 14:50:38 +0300

    less19

---
+++ less19.py
@@ -0,0 +1,26 @@
+# def count_year(birth_year, now_year=2025):
+#     print(f"You are {now_year - birth_year} years old")
+
+# count_year(1995, 2025)
+# count_year(1992)
+
+
+# def count_year(name, birth_year):
+#     print(f"{name.title()} is {2025 - birth_year} years old" )
+
+# count_year('moxirbek', 1995)
+
+
+# def kv_kub_count(number):
+#     print(f"Квадрат числа {number} равен {number**2}, куб числа {number} равен {number**3}")
+
+# kv_kub_count(2)
+
+def even_odd(number):
+    if number%2:
+        print(f"{number} это нечетное число")
+    else:
+        print(f"{number} это чётное число")
+
+even_odd(15)
+
